4fd1d55c34a1d34ca5d3b1330cf8b00d54e8acaa
sakah <h0.sakamoto@gmail.com>
2015-09-06 09:18:10 +0900

    Separete odd/even ilayer

--- main.cc
+++ main.cc
@@ -51,27 +51,53 @@ int main(int argc, char** argv)
    TVector3 mcPos;
    TVector3 mcMom;
 
-   TCanvas* c1 = new TCanvas("c1","", 500*3, 500*2);
-   c1->Divide(3,2);
+   TCanvas* c1 = new TCanvas("c1","", 500*5, 500*2);
+   c1->Divide(5,2);
 
-   TH2F* h1 = new TH2F("h1", "", 100, -100, 100, 100, -100, 100);
-   TH2F* h2 = new TH2F("h2", "", 100, -100, 100, 100, -100, 100);
-   TH2F* h3 = new TH2F("h3", "", 100, -100, 100, 100, -100, 100);
-   TH2F* h4 = new TH2F("h4", "", 100, 0, 200, 100, -150, 150);
-   TH2F* h5 = new TH2F("h5", "", 100, 0, 200, 100, -150, 150); // MeV
-   h1->SetStats(0);
-   h2->SetStats(0);
-   h3->SetStats(0);
-   h4->SetStats(0);
-   h5->SetStats(0);
+   TH2F* h11 = new TH2F("h11", "", 100, -100, 100, 100, -100, 100);
+   TH2F* h21 = new TH2F("h21", "", 100, -100, 100, 100, -100, 100);
+   TH2F* h31 = new TH2F("h31", "", 100, -100, 100, 100, -100, 100);
+   TH2F* h41 = new TH2F("h41", "", 100, 0, 200, 100, -150, 150);
+   TH2F* h51 = new TH2F("h51", "", 100, 0, 200, 100, -150, 150); // MeV
+   TH2F* h12 = new TH2F("h12", "", 100, -100, 100, 100, -100, 100);
+   TH2F* h22 = new TH2F("h22", "", 100, -100, 100, 100, -100, 100);
+   TH2F* h32 = new TH2F("h32", "", 100, -100, 100, 100, -100, 100);
+   TH2F* h42 = new TH2F("h42", "", 100, 0, 200, 100, -150, 150);
+   TH2F* h52 = new TH2F("h52", "", 100, 0, 200, 100, -150, 150); // MeV
+   h11->SetStats(0);
+   h21->SetStats(0);
+   h31->SetStats(0);
+   h41->SetStats(0);
+   h51->SetStats(0);
+   h12->SetStats(0);
+   h22->SetStats(0);
+   h32->SetStats(0);
+   h42->SetStats(0);
+   h52->SetStats(0);
 
    for (int iev=0; iev<total; iev++) {
 
-      h1->SetTitle(Form("iev %d - MC XY", iev)); c1->cd(1); h1->Draw();
-      h2->SetTitle(Form("iev %d - Wire XY @ endplate", iev)); c1->cd(2); h2->Draw();
-      h3->SetTitle(Form("iev %d - Wire XY @ hitZ", iev)); c1->cd(3); h3->Draw();
-      h4->SetTitle(Form("iev %d - MC Z vs ihit", iev)); c1->cd(4); h4->Draw();
-      h5->SetTitle(Form("iev %d - MC PZ vs ihit", iev)); c1->cd(5); h5->Draw();
+      h11->SetTitle(Form("iev %d - ODD - MC XY", iev));
+      h21->SetTitle(Form("iev %d - ODD - Wire XY @ endplate", iev));
+      h31->SetTitle(Form("iev %d - ODD - Wire XY @ hitZ", iev));
+      h41->SetTitle(Form("iev %d - ODD - MC Z vs ihit", iev));
+      h51->SetTitle(Form("iev %d - ODD - MC PZ vs ihit", iev));
+      h12->SetTitle(Form("iev %d - EVEN - MC XY", iev));
+      h22->SetTitle(Form("iev %d - EVEN - Wire XY @ endplate", iev));
+      h32->SetTitle(Form("iev %d - EVEN - Wire XY @ hitZ", iev));
+      h42->SetTitle(Form("iev %d - EVEN - MC Z vs ihit", iev));
+      h52->SetTitle(Form("iev %d - EVEN - MC PZ vs ihit", iev));
+
+      c1->cd(1); h11->Draw();
+      c1->cd(2); h21->Draw();
+      c1->cd(3); h31->Draw();
+      c1->cd(4); h41->Draw();
+      c1->cd(5); h51->Draw();
+      c1->cd(6); h12->Draw();
+      c1->cd(7); h22->Draw();
+      c1->cd(8); h32->Draw();
+      c1->cd(9); h42->Draw();
+      c1->cd(10);h52->Draw();
 
       inROOT.getEntry(iev);
       bool directHit = inROOT.InDirectHitAtTriggerCounter();
@@ -82,6 +108,7 @@ int main(int argc, char** argv)
 
       printf("iev %d numHits %d\n", iev, numHits );
       for (int ihit=0; ihit<numHits; ihit++) {
+         int ilayer = inROOT.getIlayer(ihit);
          int iturn = inROOT.getIturn(ihit);
 
          inROOT.getPosMom(ihit, mcPos, mcMom);
@@ -94,11 +121,13 @@ int main(int argc, char** argv)
          TMarker* m4 = getMarker(iturn, ihit, mcPos.Z());
          TMarker* m5 = getMarker(iturn, ihit, mcMom.Z()*1000); // GeV -> MeV
 
-         c1->cd(1); m1->Draw();
-         c1->cd(2); m2->Draw();
-         c1->cd(3); m3->Draw();
-         c1->cd(4); m4->Draw();
-         c1->cd(5); m5->Draw();
+         int offset = 0;
+         if (ilayer%2==0) offset = 5;
+         c1->cd(offset+1); m1->Draw();
+         c1->cd(offset+2); m2->Draw();
+         c1->cd(offset+3); m3->Draw();
+         c1->cd(offset+4); m4->Draw();
+         c1->cd(offset+5); m5->Draw();
 
          printf("iev %d MC:     ihit %d (%f, %f, %f)\n", iev, ihit, mcPos.X(), mcPos.Y(), mcPos.Z());
          printf("iev %d End:    ihit %d (%f, %f, %f) - (%f, %f, %f)\n", iev, ihit, w_x1, w_y1, w_z1, w_x2, w_y2, w_z2);
